1eaf0887fef8d82ff48c45efed665d28b646085b
Jennifer Slagt <45489420+jenniferslagt@users.noreply.github.com>
2021-06-04 16:28:07 +0200

    Update sw-cached-pages.js

--- assets/toolkit/scripts/sw-cached-pages.js
+++ assets/toolkit/scripts/sw-cached-pages.js
@@ -1,9 +1,41 @@
-// Call install event
-self.addEventListener('install', (e) => {
-console.log('Service Worker: Installed')
-})
+console.log('check')
 
-// Call activate event
-self.addEventListener('activate', (e) => {
-console.log('Service Worker: Activated')
-})
+
+// Call the activate event
+	self.addEventListener('activate', function (event) {
+		console.log('Service worker is actived');
+		// Remove any unwanted caches
+		// loop through the caches with a condition
+		event.waitUntil(
+			caches.keys().then(cacheNames => {
+				return Promise.all(
+					cacheNames.filter(function (cache) {
+						console.log('Service Worker cleared old cache')
+						return cache === cacheName;
+					})
+				)
+			})
+		);
+	});
+
+
+	// Call Fetch event
+	self.addEventListener('fetch', function (event) {
+		console.log('Service Worker is fetching');
+		event.respondWith(
+			fetch(event.request)
+			.then(function (res) {
+				// Make a copy / clone of the response of the server
+				const resClone = res.clone();
+				caches
+					.open(cacheName)
+					.then(function (cache) {
+						// Add the response to the cache
+						cache.put(event.request, resClone);
+					});
+				return res;
+			})
+			.catch(err => caches.match(event.request)
+				.then(res => res))
+		);
+	});
