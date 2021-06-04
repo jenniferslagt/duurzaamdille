0cb772588b24efc2465602f7f93ad15e443a8c56
Jennifer Slagt <45489420+jenniferslagt@users.noreply.github.com>
2021-06-04 16:23:52 +0200

    Test service worker

--- assets/toolkit/scripts/sw-cached-pages.js
+++ assets/toolkit/scripts/sw-cached-pages.js
@@ -1,11 +1,1 @@
-console.log('sw mapje werkt');
-
-// Call install event
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed')
-})
-
-// Call activate event
-// self.addEventListener('activate', (e) => {
-//   console.log('Service Worker: Activated')
-// })
+console.log('hai');
