c47f4677b8871be4dd1dca9951763516d3f04a19
Jennifer Slagt <45489420+jenniferslagt@users.noreply.github.com>
2021-06-04 16:25:54 +0200

    Update sw-cached-pages.js

--- assets/toolkit/scripts/sw-cached-pages.js
+++ assets/toolkit/scripts/sw-cached-pages.js
@@ -1,1 +1,9 @@
-console.log('hai');
+// Call install event
+self.addEventListener('install', (e) => {
+console.log('Service Worker: Installed')
+})
+
+// Call activate event
+self.addEventListener('activate', (e) => {
+console.log('Service Worker: Activated')
+})
